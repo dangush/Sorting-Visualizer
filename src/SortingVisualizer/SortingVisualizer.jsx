16c55312aed8b89b589eb8cc41cd2c022f17bb7a
Daniel Gushchyan <d.gushchyan@gmail.com>
2020-04-14 14:49:59 -0700

    Created buttons and stylizations

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -10,27 +10,42 @@ class SortingVisualizer extends React.Component {
     this.resetArray();
   }
 
-  resetArray() {
+  resetArray = () => {
     const array = [];
-    for (let i = 0; i < 100; i++) {
-      array[i] = getRandomArbitrary(5, 800);
+    for (let i = 0; i < 300; i++) {
+      array[i] = getRandomArbitrary(5, 600);
     }
     this.setState({ array });
-    console.log({ array });
-  }
+  };
 
   render() {
     const { array } = this.state;
     return (
-      <>
-        {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{ height: `${value}px` }}
-          ></div>
-        ))}
-      </>
+      <div>
+        <div className="array-container">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <div className="button-container">
+          <button className="button" onClick={this.resetArray}>
+            Generate New Array
+          </button>
+          <button className="button" onClick={this.resetArray}>
+            Bubble Sort
+          </button>
+          <button className="button" onClick={this.resetArray}>
+            Merge Sort
+          </button>
+          <button className="button" onClick={this.resetArray}>
+            Insertion Sort
+          </button>
+        </div>
+      </div>
     );
   }
 }
