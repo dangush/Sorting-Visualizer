e0943f3c3aec524bf9b0a18a15e9204194fd6ef6
Daniel Gushchyan <d.gushchyan@gmail.com>
2020-04-23 22:38:06 -0700

    Basic functionality added (bubble sort)

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SortingVisualizer.css";
+import { bubbleSortAlgo } from "../sortingAlgorithms/sortingAlgorithms.js";
 
 class SortingVisualizer extends React.Component {
   state = {
@@ -16,6 +17,51 @@ class SortingVisualizer extends React.Component {
       array[i] = getRandomArbitrary(5, 600);
     }
     this.setState({ array });
+  };
+
+  bubbleSort = () => {
+    const bars = document.getElementsByClassName("array-bar");
+    const changes = bubbleSortAlgo(this.state.array);
+    for (let i = 0; i < changes.length; i++) {
+      setTimeout(() => {
+        let barOneStyle = bars[changes[i][0]].style;
+        console.log("This is " + barOneStyle.height);
+        let barTwoStyle = bars[changes[i][1]].style;
+        console.log("The second is " + barTwoStyle.height);
+        let temp;
+
+        barOneStyle.backgroundColor = "red";
+        barTwoStyle.backgroundColor = "green";
+        temp = barOneStyle.height;
+        console.log("got here");
+        barOneStyle.height = barTwoStyle.height;
+        barTwoStyle.height = temp;
+
+        barOneStyle.backgroundColor = "blue";
+        barTwoStyle.backgroundColor = "blue";
+      }, i * 1);
+    }
+  };
+
+  swap(one, two) {
+    let temp = one;
+    one = two;
+    two = temp;
+
+    return one, two;
+  }
+
+  testSortAlgo = () => {
+    const sortedArray = this.state.array.slice().sort((a, b) => a - b);
+    const bubbleSort = bubbleSortAlgo(this.state.array);
+    if (sortedArray.length !== bubbleSort.length) return false;
+    for (let i = 0; i < sortedArray.length; i++) {
+      if (sortedArray[i] !== bubbleSort[i]) {
+        console.log("NOPE");
+        return false;
+      }
+    }
+    console.log("NICE");
   };
 
   render() {
@@ -35,8 +81,8 @@ class SortingVisualizer extends React.Component {
           <button className="button" onClick={this.resetArray}>
             Generate New Array
           </button>
-          <button className="button" onClick={this.resetArray}>
-            TEST Sort
+          <button className="button" onClick={this.bubbleSort}>
+            Bubble Sort
           </button>
           <button className="button" onClick={this.resetArray}>
             Merge Sort
